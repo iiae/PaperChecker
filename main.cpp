8ff47657dc802cbe440de297fb7205a560b48968
Axoford12 <847072154@qq.com>
2017-07-12 17:23:45 +0800

    Finished!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
-//
-// Created by Axoford12 on 2017/7/12.
-//
-#include <iostream>
-int main(int argc,char* argv[]){
 
+#include <cstdio>
+#include <io.h>
+
+int main() {
+    freopen("answer.txt", "r", stdin);
+    int answerNumber, questionPoints;
+    char *answer = new char[answerNumber + 1];
+    scanf("%d", &answerNumber);
+    scanf("%d", &questionPoints);
+    scanf("%s", answer);
+    long paperLength = filelength(open("paper.txt", 0x0100));
+    char *paper = new char[paperLength + 1];
+    freopen("paper.txt", "r", stdin);
+    scanf("%s", paper);
+    int i = 0;
+    int score = 0;
+    while (answer[i] != '\0') {
+        if (paper[i] == answer[i] && i < paperLength) score += questionPoints;
+        i++;
+    }
+    printf("%d", score);
 }
